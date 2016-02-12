6058393b46c3c71fa13f43d3d661cbd9b094e1bb
Diogo Marques <diogohomemmarques@gmail.com>
2016-02-12 20:28:44 +0000

    improved vis

--- playground/rrtating_test.R
+++ playground/rrtating_test.R
@@ -3,21 +3,33 @@ n = 1000
 mean = 3.5
 mean = 4
 mean = 2.1
-sd = 1
+sd = 10/4
 scale.from = 1
-scale.to = 5
+scale.to = 10
 shift.to.mean = "none"
 shift.to.mean = "quick"
 
-par(mfrow=c(2,1))
-
-responses.none = rrating(n = n, mean = mean, scale.from = 1, scale.to = 5, sd.norm = sd, shift.to.mean = "none")
-plot(table(responses.none))
+responses.none = rrating(n = n, mean = mean, scale.from = scale.from,
+                         scale.to = scale.to, sd.norm = sd,
+                         shift.to.mean = "none")
 mean(responses.none)
 sd(responses.none)
 
-responses.quick = rrating(n = n, mean = mean, scale.from = 1, scale.to = 5, sd.norm = sd, shift.to.mean = "quick")
-plot(table(responses.quick))
+
+
+responses.quick = rrating(n = n, mean = mean, scale.from = scale.from,
+                          scale.to = scale.to, sd.norm = sd,
+                          shift.to.mean = "quick")
 mean(responses.quick)
 sd(responses.quick)
+table.responses.quick = table(responses.quick)
+dimnames(table.responses.quick) = list(as.numeric(dimnames(table.responses.quick)[[1]]) + 0.05)
+dimnames(table.responses.quick)
 
+
+plot(table(responses.none))
+abline(v = mean(responses.none), lty = 2)
+axis(3, at = mean(responses.none), label = "mean - none")
+points(table.responses.quick, col = "red")
+abline(v = mean(responses.quick), lty = 2, col = "red")
+axis(1, at = mean(responses.quick), label = "mean - quick", col = "red")
